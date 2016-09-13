22ec71ad9d1dfe0ab1daea34ea0ab3bce9fb1035
Barnaby Claydon <barnaby@swipely.com>
2016-09-12 23:45:19 -0400

    Move decoders to own module

--- src/components/communication_center/communication-center.elm
+++ src/components/communication_center/communication-center.elm
@@ -7,10 +7,9 @@ import Html.Events exposing (..)
 import Date
 
 import AuthenticatedHttp exposing (get, post)
-import Json.Decode as Json exposing ((:=))
-import Json.Decode.Extra as JsonExtra
 
-import Types exposing (Model, Message, Replies, User, Msg(..), Metadata, ReadReceipt, Attachment, Image)
+import Types exposing (Model, Message, Replies, User, Msg(..))
+import Decoders exposing (decodeMessages)
 import Formatters exposing (formatDate, formatRecipients, formatReplies)
 import DummyData exposing (dummyModel, dummyMessage, dummyReply)
 
@@ -35,8 +34,8 @@ model : Model
 model =
   dummyModel
 
--- UPDATE
 
+-- UPDATE
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -71,7 +70,6 @@ subscriptions model =
 
 -- HTTP
 
-
 getMessages : Cmd Msg
 getMessages =
   get
@@ -86,70 +84,10 @@ postMessage body =
   post
     "foo"
     "/v1/message"
-    """{ "body": "foo" }""" -- todo: proper Json encode
+    """{ "body": "foo" }"""
     PostFail
     PostSucceed
     decodeMessages
-
-decodeUser : Json.Decoder User
-decodeUser =
-  Json.object2
-    User
-      ("uuid" := Json.string)
-      ("name" := Json.string)
-
-decodeReplies : Json.Decoder Replies
-decodeReplies =
-  Json.object1
-    Types.Replies decodeMessages
-
-decodeReadreceipts : Json.Decoder (List ReadReceipt)
-decodeReadreceipts =
-  Json.list (
-    Json.object2
-      ReadReceipt
-        ("author" := decodeUser)
-        ("created" := JsonExtra.date)
-  )
-
-decodeRecipients : Json.Decoder (List User)
-decodeRecipients =
-  Json.list decodeUser
-
-decodeMetadata : Json.Decoder Metadata
-decodeMetadata =
-  Json.object5
-    Metadata
-      ("readReceipts" := decodeReadreceipts )
-      ("author" := decodeUser)
-      ("recipients" := decodeRecipients )
-      ("created" := JsonExtra.date)
-      (Json.maybe ( "updated" := JsonExtra.date))
-
-decodeImage : Json.Decoder Image
-decodeImage =
-  Json.object2
-    Image
-      ("url" := Json.string)
-      ("description" := Json.string)
-
-decodeMessages : Json.Decoder (List Message)
-decodeMessages =
-  Json.list (
-    Json.object5
-      Message
-        ("subject" := Json.string)
-        ("body" := Json.string)
-        ("attachments" := Json.list (
-          Json.object2
-            Attachment
-              ("image" := decodeImage)
-              ("meta" := decodeMetadata)
-          )
-        )
-        ("replies" := decodeReplies)
-        ("meta" := decodeMetadata)
-  )
 
 
 -- VIEW
---
+++ src/components/communication_center/decoders.elm
@@ -0,0 +1,65 @@
+module Decoders exposing (..)
+
+import Types exposing (Message, Replies, User, Metadata, ReadReceipt, Attachment, Image)
+import Json.Decode as Json exposing ((:=))
+import Json.Decode.Extra as JsonExtra
+
+decodeUser : Json.Decoder User
+decodeUser =
+  Json.object2
+    User
+      ("uuid" := Json.string)
+      ("name" := Json.string)
+
+decodeReplies : Json.Decoder Replies
+decodeReplies =
+  Json.object1
+    Types.Replies decodeMessages
+
+decodeReadreceipts : Json.Decoder (List ReadReceipt)
+decodeReadreceipts =
+  Json.list (
+    Json.object2
+      ReadReceipt
+        ("author" := decodeUser)
+        ("created" := JsonExtra.date)
+  )
+
+decodeRecipients : Json.Decoder (List User)
+decodeRecipients =
+  Json.list decodeUser
+
+decodeMetadata : Json.Decoder Metadata
+decodeMetadata =
+  Json.object5
+    Metadata
+      ("readReceipts" := decodeReadreceipts )
+      ("author" := decodeUser)
+      ("recipients" := decodeRecipients )
+      ("created" := JsonExtra.date)
+      (Json.maybe ( "updated" := JsonExtra.date))
+
+decodeImage : Json.Decoder Image
+decodeImage =
+  Json.object2
+    Image
+      ("url" := Json.string)
+      ("description" := Json.string)
+
+decodeMessages : Json.Decoder (List Message)
+decodeMessages =
+  Json.list (
+    Json.object5
+      Message
+        ("subject" := Json.string)
+        ("body" := Json.string)
+        ("attachments" := Json.list (
+          Json.object2
+            Attachment
+              ("image" := decodeImage)
+              ("meta" := decodeMetadata)
+          )
+        )
+        ("replies" := decodeReplies)
+        ("meta" := decodeMetadata)
+  )
